a2c9129b6a6911ee619d67ca8d400c6237c37aa8
Caio Pessoa <caiopessoa1298@gmail.com>
2019-11-02 16:52:37 -0300

    Criando arquivo Aula01_Varibles.swift onde contem o conteúdo da aula 1

---
+++ Aula01_Variables.swift
@@ -0,0 +1,25 @@
+//: Playground - noun : a place where can play
+
+var str = "Hello, playground"
+
+print(str)
+
+var a = 1
+var b = 1
+
+a = 2
+
+str = "hello"
+
+a = b
+
+var myNumberOfApples = 19
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+let c = 10
+
+//c = 2// ERROR
