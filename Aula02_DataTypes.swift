6562073513a6ecaba6e11ad3f417b576831685d2
Caio Pessoa <caiopessoa1298@gmail.com>
2019-11-02 21:56:20 -0300

    Criando arquivo Aula02_DataTypes.swift onde contem o conteúdo da aula 2

---
+++ Aula02_DataTypes.swift
@@ -0,0 +1,41 @@
+//: Playground - noun : a place where can play
+
+var str:String = "Hello, playground"
+
+print(str)
+
+var a:Int = 1
+var b:Int = 1
+
+a = 2
+
+str = "hello"
+
+a = b
+
+//str = 29 //ERROR Becouse de DataTypes are differents
+str = String(29)
+
+var myNumberOfApples = 19
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+let f = 10
+
+var caio:String
+var name = "caio"
+
+var c:Float = 2.3
+var d:Double = 13.9
+var e:Bool = true
+print("\nDataTypes\n")
+print(c, "Float to int", Int(c))
+print(d, "Double to int", Int(d))
+//print(d, "Double to Round", int(round(d))//round simplifica o double onde fica melhor
+
+
+
+//c = 2// ERROR
